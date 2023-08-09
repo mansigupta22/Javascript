00e5cf8ec9fe6b206230bc1a4cf119f62849a5dc
mansigupta22 <110581261+mansigupta22@users.noreply.github.com>
2023-08-09 15:22:03 +0000

    conversion Operators

--- 01basics/03conversionOperation.js
+++ 01basics/03conversionOperation.js
@@ -3,27 +3,65 @@
 // let age = null // 0
 // let age = undefined // NaN
 // let age = true // => 1 // false => 0
-let age = "Mansi" // => NaN
+// let age = "Mansi" // => NaN
 
 
 /* const {score} = req.body */
-console.log(typeof age);
-console.log(typeof (age));
+// console.log(typeof age);
+// console.log(typeof (age));
 
-let valueInNumber = Number(age)
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// let valueInNumber = Number(age)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 
 // let isLoggedIn = 1 //true
 // let isLoggedIn = 0 //false
 // let isLoggedIn = "" // false
-let isLoggedIn = "Mansi" //true
+// let isLoggedIn = "Mansi" //true
 
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn)
 
-let someNumber = 33
+// let someNumber = 33
 
-let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+//***************Operations*****************/
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log (2+2)  // addition
+// console.log (2-2)  // subtraction
+// console.log (2*2)  // multiplication
+// console.log (2**2) // power
+// console.log (2/2) // division
+// console.log (2%2) // modulus
+
+// let str1 = "Hello"
+// let str2 = " Mansi"
+// let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+// /* ToPrimitive 7.1.1 refer to ecma */
+
+// console.log(true); // true
+// console.log(+true); // 1, not suitable way of writing code
+// console.log(true+); // error
+// console.log(+""); // 0, tricky and also not suitable way
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2 + 2 // not good way // code readability is imp
+// console.log(num2);
+
+let gameCounter = 100
+// ++gameCounter; // prefix
+gameCounter++; // postfix
+console.log(gameCounter);
