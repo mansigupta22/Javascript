3661457d1762b6761e41ef1e1d7169f1cd004c24
Mansi <110581261+mansigupta22@users.noreply.github.com>
2023-08-17 09:02:03 +0000

    scope level and hoisting

--- 03basics/02scopes.js
+++ 03basics/02scopes.js
@@ -16,13 +16,55 @@ let a = 300 // global scope
 if (true){
     let a = 10
     const b = 20
-    console.log("Inner:", a); // local scope
+    // console.log("Inner:", a); // local scope
 }
-console.log(a);
+// console.log(a);
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
+// for (let i = 0; i < array.length; i++) {
+//     const element = array[i];
     
-}
+// }
 
 // scope is different in node environment and different in browser environment
+
+
+// *******nested scope*******//
+// child can access parent value
+
+function one(){
+     const username = "Mansi"
+
+     function two(){
+        const website = "github"
+        console.log(username);
+     }
+    //  console.log(website);
+
+    //  two()
+
+}
+
+// one()
+
+if(true){
+    const username = "Mansi"
+    if(username === "Mansi"){
+        const website = " leetcode"
+        // console.log(username + website);
+    }
+    // console.log(website); // can't access bcz it's outside it's scope
+}
+// console.log(username); // outside scope
+
+//**********intresting**********//
+
+console.log(addone(5));
+function addone(num){
+    return num + 1
+}
+
+console.log(addTwo(5)) // can't access before initialization
+const addTwo = function(num){
+    return num + 2
+}
+
