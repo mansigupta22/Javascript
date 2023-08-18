10a94e3e0668fe91d53e7f295f714aa0545ffe72
Mansi <110581261+mansigupta22@users.noreply.github.com>
2023-08-18 21:32:17 +0000

    switch case

---
+++ 04controlflow/02switch.js
@@ -0,0 +1,50 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = 3
+
+switch (month) {
+    case 1:
+        console.log("january");
+        break;
+    case 2:
+        console.log("feb");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("april");
+        break;
+
+    default:
+        console.log("after april");
+        break;
+}
+
+const month1 = "feb"
+
+switch (month1) {
+    case "jan":
+        console.log("january");
+        break;
+    case "feb":
+        console.log("feb");
+        break;
+    case "march":
+        console.log("march");
+        break;
+    case "april":
+        console.log("april");
+        break;
+
+    default:
+        console.log("after april");
+        break;
+}
